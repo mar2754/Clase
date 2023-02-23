6795848667a2ac8a3c9bf6ee249b447ca7afe617
munoz <munozsanchezmariana0@gmail.com>
2023-02-23 01:43:56 -0500

    Implement commit

---
+++ ejercicios/clase.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, age, grades):
+        self.name = name
+        self.age = age
+        self.grades = grades
+
+class add_grade:
+    def __init__(self):
+        self.numero = []
+
+    def agregar_grades(self, numero):
+        self.numero
+
+class average_grade:
+    def __init__(self , promedio):
+        self.promedio
